e3ca1cd1af5e3eec6671ca38e73c675f1578033d
Terrence Kuiper <github@terrencekuiper.com>
2019-10-18 22:22:03 +0000

    Fixing initial codebase

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,7 @@ $(() => {
     method: "GET",
     url: "/api/users"
   }).done((users) => {
-    for(user of users) {
+    for (user of users.users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
