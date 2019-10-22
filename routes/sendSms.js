39ed2f545581ec7632fdfe3a6bb221881323439c
Terrence Kuiper <github@terrencekuiper.com>
2019-10-22 10:36:01 -0700

    Use the Twilio dev credentials

--- routes/sendSms.js
+++ routes/sendSms.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = process.env.DEV_TWILIO_ACCOUNT_SID;
+const authToken = process.env.DEV_TWILIO_AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
 
@@ -12,8 +12,8 @@ const sendMessage = function() {
   return client.messages
     .create({
       body: orderReceivedMessage,
-      from: process.env.OUTGOING_PHONE_NUMBER,
-      to: process.env.TEST_PHONE_NUMBER
+      from: process.env.DEV_OUTGOING_PHONE_NUMBER,
+      to: process.env.DEV_TEST_PHONE_NUMBER
     })
     .then((message) => console.log(message.sid))
     .catch(err => console.log('ERROR', err));
