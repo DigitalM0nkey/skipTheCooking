7d141dc91d4d3eb67641af64731655df9db7e947
Terrence Kuiper <github@terrencekuiper.com>
2019-10-23 18:35:01 -0700

    Add shopping cart items to the database as a new order

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require('morgan');
 const cookieSession = require('cookie-session');
-
+const async = require('async');
 
 app.use(cookieSession({
   name: 'session',
@@ -73,25 +73,47 @@ app.get('/login/:id', (req, res) => {
   res.redirect('/');
 });
 
+
+
+app.post('/checkout', (req, res) => {
+  // This returns an ID#
+  const newOrderId = createOrder(req.session.user_id)
+    .then(resultOfCreateOrder => {
+      console.log(resultOfCreateOrder);
+      orderedItems(resultOfCreateOrder.rows[0].id, req.session.cart);
+    })
+    .catch(err => console.log('OH NO orderedItems ', err.stack)
+    );
+  // This takes in the id from the other function
+  //orderedItems(newOrderId.id, req.session.cart);
+
+});
+
+// console.log('newOrderId = ', newOrderId);
+// console.log("herro", req.session.cart);
+// console.log("req Session", req.session);
+
+
+
+
 app.post("/addToCart", (req, res) => {
   if (req.session.cart === undefined) {
     req.session.cart = req.body.itemId;
   } else {
-    let cart = req.session.cart.concat(req.body.itemId)
+    let cart = req.session.cart.concat(req.body.itemId);
     req.session.cart = cart;
   }
-  console.log("req session after all funcs: ", req.session.cart)
+  console.log("req session after all funcs: ", req.session.cart);
   res.json({ "cart": req.session.cart });
 });
 
 app.get("/clear-cart", (req, res) => {
   console.log("clear cart now!")
-  console.log("cart before clear", req.session.cart)
+  console.log("cart before clear", req.session.cart);
   req.session.cart = [];
-  res.redirect("/")
-  console.log("cart after clear", req.session.cart)
-
-})
+  res.redirect("/");
+  console.log("cart after clear", req.session.cart);
+});
 
 app.listen(PORT, () => {
   console.log(`skipTheCooking app listening on port ${PORT}`);
@@ -99,30 +121,28 @@ app.listen(PORT, () => {
 
 //DATBASE CALLS
 
-const foodQuery = () => {
-  db.query('SELECT * FROM foods;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    db.end();
-  });
-};
+// const foodQuery = () => {
+//   db.query('SELECT * FROM foods;', (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//       console.log(JSON.stringify(row));
+//     }
+//     db.end();
+//   });
+// };
 
 //foodQuery();
 
 const createOrder = function(userID) {
   const text = `INSERT INTO orders(user_id, timestamp) VALUES(${userID},  NOW()) RETURNING id`;
-  db.query(text)
-    .then(res => console.log(res.rows[0]))
-    .catch(err => console.log('OH NO orderedItems ', err.stack)
-    );
+  return db.query(text);
 };
 
 //createOrder(3);
 
 const sendToDatabase = (foodId, orderId, qty) => {
   let queryText = `INSERT INTO ordered_items(food_id,order_id,qty) VALUES(${parseInt(foodId)},${orderId},${qty}) RETURNING id;`;
+  console.log("quesryText = ", queryText);
 
   return db.query(queryText);
 
@@ -146,11 +166,9 @@ const orderedItems = function(orderId, cookie) {
       console.log("Promises complete");
     })
     .catch(err => console.log('OH NO orderedItems ', err.stack));
-
-
 };
 
 
 
 
-orderedItems(3, ['1', '1', '3', '5', '5']);
+//orderedItems(3, ['1', '1', '3', '5', '5']);
